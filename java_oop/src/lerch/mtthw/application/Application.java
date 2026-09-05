fb32f22230b7e1d12ab110bca64df635edc9cb83
marchelrn <marchelmanullang026@student.unsrat.ac.id>
2026-09-05 19:48:33 +0800

    learn import

--- java_oop/src/lerch/mtthw/application/Application.java
+++ java_oop/src/lerch/mtthw/application/Application.java
@@ -1,11 +1,13 @@
 package lerch.mtthw.application;
 
-import lerch.mtthw.data.Product;
+import lerch.mtthw.data.*;
 
 public class Application {
     static void main(String[] args) {
         Product product = new Product("Mac Book Pro", 3000000);
         System.out.println(product.name);
         System.out.println(product.name);
+
+        Data data = new Data();
     }
 }
---
+++ java_oop/src/lerch/mtthw/data/Data.java
@@ -0,0 +1,5 @@
+package lerch.mtthw.data;
+
+public class Data {
+
+}
