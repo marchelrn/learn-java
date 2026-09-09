b07fff6b87f6acfdd81e7f3222938ae6b047b7fc
marchelrn <marchelmanullang026@student.unsrat.ac.id>
2026-09-10 00:00:24 +0800

    learn final class

---
+++ java_oop/src/lerch/mtthw/data/SocialMedia.java
@@ -0,0 +1,14 @@
+package lerch.mtthw.data;
+
+class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+// ERROR
+//class FakeFacebook extends Facebook {
+//
+//}
