6c3f1659eeadcc20b076aa4daf3c1496b5386b75
marchelrn <marchelmanullang026@student.unsrat.ac.id>
2026-09-14 21:54:01 +0800

    learn enum class

---
+++ java_oop/src/lerch/mtthw/application/EnumApp.java
@@ -0,0 +1,28 @@
+package lerch.mtthw.application;
+
+import jdk.swing.interop.SwingInterOpUtils;
+import lerch.mtthw.data.Customer;
+import lerch.mtthw.data.Level;
+
+public class EnumApp {
+    static void main(String[] args) {
+        Customer customer = new Customer();
+
+        customer.setName("Marchel");
+        customer.setLevel(Level.STANDARD);
+
+        System.out.println(customer.getName());
+        System.out.println(customer.getLevel());
+        System.out.println(customer.getLevel().getDescription());
+
+        String levelName = Level.STANDARD.name();
+        System.out.println(levelName); // Convert menjadi String
+
+        Level level = Level.valueOf("PREMIUM");
+        System.out.println(level); // Convert menjadi enum
+
+        for (var value : Level.values()){
+            System.out.println(value); // mengembalikan seluruh isi dari enum
+        }
+    }
+}
